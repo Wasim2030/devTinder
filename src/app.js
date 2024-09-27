0f4c59e29a3f65b45e5484c00f0f9a51c61e27b8
wasim <wasimakhter2030@gmail.com>
2024-09-27 19:40:54 +0530

    ep 4 p2 get post put delete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello/2", (req, res) => {
-  res.send("sequence of code matters");
+// Order matters
+// app.use("/user", (req, res) => {
+//   res.send("Order matters");
+// });
+
+// this will only handle Get call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstNmae: "Wasim", lastName: "Akhter" });
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello ello llo");
+app.post("/user", (req, res) => {
+  //saving data to database
+  res.send("Data successfully saved to the database");
 });
 
+// making delete call
+app.delete("/user", (req, res) => {
+  //saving data to database
+  res.send("deleted successfully");
+});
+
+// This will match all the HTTP method api calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
